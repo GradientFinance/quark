b9f6284825ca10aba9ce357fd82b9a409c320ddc
cairo <cairoeth@protonmail.com>
2022-11-05 17:12:22 -0700

    fix deploy scripts

--- contracts/script/deploy/DeployBase.s.sol
+++ contracts/script/deploy/DeployBase.s.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.15;
 
 import "forge-std/Script.sol";
 
 import {LibRLP} from "../../test/utils/LibRLP.sol";
 import {IndexFactory} from "../../src/IndexFactory.sol";
+import {Exchange} from "../../src/Exchange.sol";
+import {Key} from "./Key.sol";
 
-abstract contract DeployBase is Script {
+abstract contract DeployBase is Script, Key {
     // Environment specific variables.
     address[] private denominations;
     address private uma;
-
-    // Deploy addresses.
-    IndexFactory public _indexfactory;
 
     constructor(
         address[] memory _denominations,
@@ -23,14 +22,14 @@ abstract contract DeployBase is Script {
     }
 
     function run() external {
-        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerKey = private_key;
         address DeployerAddress = vm.addr(deployerKey);
 
-        vm.startBroadcast(deployerKey);
+        vm.startBroadcast(DeployerAddress);
 
-        // Deploy team and community reserves, owned by cold wallet.
-        _indexfactory = new IndexFactory();
-        // _exchange = new Exchange(address(_indexfactory));
+        // Deploy factory and exchange contracts.
+        IndexFactory _indexfactory = new IndexFactory(denominations, uma);
+        Exchange _exchange = new Exchange(address(_indexfactory));
         
         vm.stopBroadcast();
     }
